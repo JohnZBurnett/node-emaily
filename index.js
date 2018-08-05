0a9220983321d7d5187091a8e07d13a3e5e77a2c
JohnZBurnett <tuninator@gmail.com>
2018-08-05 17:48:05 -0400

    implement current_user and logout

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-
-
+const passport = require('passport'); 
+const cookieSession = require('cookie-session');
 const express = require('express');
 const mongoose = require('mongoose'); 
+const keys = require('./config/keys'); 
+const PORT = process.env.PORT || 5000;
 require('./models/User'); 
 require('./services/passport');
-const keys = require('./config/keys')
-
 
 const app = express(); 
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+); 
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 
 require('./routes/authRoutes')(app); 
 
 mongoose.connect(keys.mongoURI); 
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT)
+
