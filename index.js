00802b683fa4486243ddca90179bd2691ad202fa
JohnZBurnett <tuninator@gmail.com>
2018-08-04 21:34:08 -0400

    create user model

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require('./models/User'); 
+
 const express = require('express');
+const mongoose = require('mongoose'); 
+const passportConfig = require('./services/passport');
+const keys = require('./config/keys')
+
+
 const app = express(); 
 
+require('./routes/authRoutes')(app); 
+
+mongoose.connect(keys.mongoURI); 
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send({ salut: "mon ami"})
-});
-
 app.listen(PORT)
