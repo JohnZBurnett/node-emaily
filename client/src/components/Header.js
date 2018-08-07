be9e83f455ae06b9503844d940ca8242ffb0e53d
JohnZBurnett <tuninator@gmail.com>
2018-08-07 17:44:10 -0400

    implementing auth actions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'; 
+import { connect } from 'react-redux'; 
 
 class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+              return "Still deciding";
+            case false:
+              return "I'm logged out"
+            default:
+              return "I'm logged in"; 
+        }
+    }
+
     render() {
+        console.log("Header Render: ", this.props); 
         return (
             <nav>
                 <div className="nav-wrapper">
                   <a className="left brand-logo">Emaily</a>
                   <ul className="right">
-                    <li>
-                        <a>Login with Google</a>
-                    </li>
+                    {this.renderContent()} 
                   </ul>
                 </div>
             </nav>
@@ -17,4 +28,8 @@ class Header extends Component {
     }
 }
 
-export default Header; 
+function mapStateToProps({auth}) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header); 
