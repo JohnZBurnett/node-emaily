0732c0bad458f66ea8d6f6bddc6ff7c8f27184b1
JohnZBurnett <tuninator@gmail.com>
2018-08-13 10:38:09 -0400

    add login pathing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,12 @@ module.exports = (app) => {
         scope: ['profile', 'email']
     }));
 
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google'), 
+        (req, res) => {
+            console.log("DOES THIS PRINT TO CONSOLE?"); 
+            res.redirect('/surveys'); 
+        }
+);
     
     app.get('/api/logout', (req, res) => {
         req.logout(); 
