34e72031f676cefb61e13eae46bb8bd3a1dd10af
JohnZBurnett <tuninator@gmail.com>
2018-09-18 15:26:52 -0400

    import Recipient schema to Survey

--- models/Recipient.js
+++ models/Recipient.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose'); 
 const { Schema } = mongoose; 
 
+// this will be a sub-document for Survey
+
 const recipientSchema = new Schema({
-    
+    email: String,
+    responded: { type: Boolean, default: false }
 })
+
+module.exports = recipientSchema; 
