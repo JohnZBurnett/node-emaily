0710609aea7ebb7ace0b8e528b1718fda4761929
JohnZBurnett <tuninator@gmail.com>
2018-10-19 13:02:16 -0400

    write SurveyForm boilerplate

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// SurveyForm shows a form for a user to add input 
+class SurveyForm extends Component {
+    render() {
+        return(
+            <div>
+                SurveyForm
+            </div>
+        ); 
+    }
+}
+
+export default SurveyForm; 
