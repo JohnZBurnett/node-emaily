9659604d5e7a598683b1c98101e878063819115f
JohnZBurnett <tuninator@gmail.com>
2018-10-19 12:56:24 -0400

    write SurveyNew.js boilerplate

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import { React, Component } from 'react';
+
+class surveyNew extends Component {
+    render() {
+        return(
+            <div>
+                SurveyNew
+            </div>
+        ); 
+    }
+}
+
+export default surveyNew; 
