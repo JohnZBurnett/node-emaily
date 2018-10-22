144b17d12850229fbc31f7e0eceede3518af882d
JohnZBurnett <tuninator@gmail.com>
2018-10-21 22:14:03 -0400

    spread props onto input element of SurveyField.js

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 
 // SurveyField contains logic to render a single label and text input
-export default () => {
+export default ({ input }) => {
     return(
         <div>
-            <input />
+            <input {...input}/>
         </div>
     )
 }
-
-/*
- <Field 
-                    type="text"
-                    name="surveyTitle"
-                    component="input"
-                    />
-*/
