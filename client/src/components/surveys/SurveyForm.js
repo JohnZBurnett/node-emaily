17b4abcefb8fcdc65d12d6125d50624a7982f9e7
JohnZBurnett <tuninator@gmail.com>
2018-10-23 15:47:37 -0400

    programatically generate SurveyFields in renderFields fn within SurveyForm

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,38 +1,23 @@
 import React, { Component } from 'react';
 import  { reduxForm, Field} from 'redux-form'; 
 import SurveyField from './SurveyField'; 
+import _ from 'lodash'; 
+
+const FIELDS = [
+    { label: "Survey Title", name: "title" },
+    { label: "Subject Line", name: "subject" },
+    { label: "Email Body", name: "body" },
+    { label: "Recipient List", name: "emails"}
+]; 
 
 // SurveyForm shows a form for a user to add input 
 class SurveyForm extends Component {
 
     renderFields() {
         return(
-            <div>
-                <Field 
-                  type="text" 
-                  name="title" 
-                  label="Survey Title" 
-                  component={SurveyField} 
-                />
-                <Field 
-                  type="text" 
-                  name="title" 
-                  label="Subject Line" 
-                  component={SurveyField} 
-                />
-                <Field 
-                  type="text" 
-                  name="title" 
-                  label="Email Body" 
-                  component={SurveyField} 
-                />
-                <Field 
-                  type="text" 
-                  name="title" 
-                  label="Recipient List" 
-                  component={SurveyField} 
-                />
-            </div>
+            _.map(FIELDS, ({label, name}) => {
+                return <Field component={SurveyField} type="text" label={label} name={name} />
+            })
         );
     }
     render() {
