07ab42a75a469d326be6d0e875761a270a67cca9
JohnZBurnett <tuninator@gmail.com>
2018-10-23 19:40:25 -0400

    check for error & touch in SurveyField.js

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 // SurveyField contains logic to render a single label and text input
-export default ({ input, label}) => {
+export default ({ input, label, meta: { error, touched }}) => {
+    console.log("META: ", meta);
     return(
         <div>
             <label>{label}</label>
             <input {...input}/>
+            {touched && error ? error : null}
         </div>
     )
 }
