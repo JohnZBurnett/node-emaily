0c6e0e1d3dd47e5ae81c18217cd6ed26ac194655
JohnZBurnett <tuninator@gmail.com>
2018-10-24 11:48:32 -0400

    Add state to SurveyNew

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm'; 
+import SurveyFormReview from './SurveyFormReview'; 
 
 // SurveyNew shows SurveyForm and SurveyFormReview
 class SurveyNew extends Component {
+
+    state = {
+        formReview: false
+    }; 
+    
     render() {
         return(
             <div>
